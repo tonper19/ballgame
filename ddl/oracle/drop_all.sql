47b77082ab9d9700edb8bbf137b06e4ed879d333
Tony Perez <tonper19@gmail.com>
2023-12-29 12:42:58 +0100

    BSD-3 License

--- ddl/oracle/drop_all.sql
+++ ddl/oracle/drop_all.sql
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2023, Antonio Alberto Pérez Pérez
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
 -- c##baseball
 -- DROP ALL
 drop table c##baseball.fact_batting_stat;
