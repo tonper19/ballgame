685424bb707f8476b84825649b978ac3e509f17d
Tony Perez <tonper19@gmail.com>
2024-04-06 11:46:13 +0200

    gitpull script (windows)

--- bi/scripts/gitpull.bat
+++ bi/scripts/gitpull.bat
@@ -8,3 +8,4 @@ pause
 :: to run this from a scheduled task with output
 :: appended to a text file enter this in the task
 :: action /C c:\ballgame\gitpull.bat >> c:\ballgame\gitpull.output.txt 2>&1
+:: put the pause command in comments if scheduling this script.
