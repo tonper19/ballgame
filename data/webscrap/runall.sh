9a8de1175db3e3d27f552eb1c84340380dc02a53
Tony Perez <tonper19@gmail.com>
2024-04-30 10:49:15 +0200

    webscraping runall initial

---
+++ data/webscrap/runall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d1/stats/general/all/batting ../csv/stats_batting_raw_d1_2024.csv
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d1/stats/general/all/pitching ../csv/stats_pitching_raw_d1_2024.csv
+#npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d1/stats/general/all/fielding ../csv/stats_fielding_raw_d1_2024.csv
