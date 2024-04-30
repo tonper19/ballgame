4934547163e123924de2e1427a9d86c33e95468b
Tony Perez <tonper19@gmail.com>
2024-04-30 19:47:39 +0200

    New webscraping

--- data/webscrap/runall.sh
+++ data/webscrap/runall.sh
@@ -1,39 +1,207 @@
 #!/bin/bash
 clear
 printf "#########################\n"
-printf "# Belgium               #\n"
+printf "# Belgium Baseball      #\n"
 printf "#########################\n"
 
-printf "\n "
-printf "##\n"
-printf "## D1\n"
-printf "##\n"
+printf "\n"
+printf "########\n"
+printf "## D1 ##\n"
+printf "########\n"
 
-printf "\n "
-printf "# 2024\n"
+printf "\n"
+printf "#### BE D1 2024\n"
 npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d1/stats/general/all/batting ../csv/stats_batting_raw_d1_2024.csv
 npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d1/stats/general/all/pitching ../csv/stats_pitching_raw_d1_2024.csv
 npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d1/stats/general/all/fielding ../csv/stats_fielding_raw_d1_2024.csv
 
-printf "\n "
-printf "# 2023\n"
+printf "\n"
+printf "#### BE D1 2023\n"
 npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2023-baseball-d1/stats/general/all/batting ../csv/stats_batting_raw_d1_2023.csv
 npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2023-baseball-d1/stats/general/all/pitching ../csv/stats_pitching_raw_d1_2023.csv
 npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2023-baseball-d1/stats/general/all/fielding ../csv/stats_fielding_raw_d1_2023.csv
 
-printf "\n "
-printf "# 2022\n"
-npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2022-baseball-d1/stats/general/all/batting ../csv/stats_batting_raw_d1_2022.csv
-npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2022-baseball-d1/stats/general/all/pitching ../csv/stats_pitching_raw_d1_2022.csv
-npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2022-baseball-d1/stats/general/all/fielding ../csv/stats_fielding_raw_d1_2022.csv
+# printf "\n"
+# printf "#### BE D1 2022\n"
+# npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2022-baseball-d1/stats/general/all/batting ../csv/stats_batting_raw_d1_2022.csv
+# npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2022-baseball-d1/stats/general/all/pitching ../csv/stats_pitching_raw_d1_2022.csv
+# npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2022-baseball-d1/stats/general/all/fielding ../csv/stats_fielding_raw_d1_2022.csv
 
-printf "\n "
-printf "# 2021\n"
+printf "\n"
+printf "#### BE D1 2021\n"
 npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2021-baseball-d1/stats/general/all/batting ../csv/stats_batting_raw_d1_2021.csv
 npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2021-baseball-d1/stats/general/all/pitching ../csv/stats_pitching_raw_d1_2021.csv
 npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2021-baseball-d1/stats/general/all/fielding ../csv/stats_fielding_raw_d1_2021.csv
 
+printf "\n"
+printf "########\n"
+printf "## D2 ##\n"
+printf "########\n"
 
+printf "\n"
+printf "#### BE D2 2024\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d2/stats/general/all/batting ../csv/stats_batting_raw_d2_2024.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d2/stats/general/all/pitching ../csv/stats_pitching_raw_d2_2024.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d2/stats/general/all/fielding ../csv/stats_fielding_raw_d2_2024.csv
+
+printf "\n"
+printf "#### BE D2 2023\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2023-baseball-d2/stats/general/all/batting ../csv/stats_batting_raw_d2_2023.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2023-baseball-d2/stats/general/all/pitching ../csv/stats_pitching_raw_d2_2023.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2023-baseball-d2/stats/general/all/fielding ../csv/stats_fielding_raw_d2_2023.csv
+
+
+printf "\n"
+printf "########\n"
+printf "## D3 ##\n"
+printf "########\n"
+
+printf "\n"
+printf "#### BE D3 2024\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d3/stats/general/all/batting ../csv/stats_batting_raw_d3_2024.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d3/stats/general/all/pitching ../csv/stats_pitching_raw_d3_2024.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d3/stats/general/all/fielding ../csv/stats_fielding_raw_d3_2024.csv
+
+printf "\n"
+printf "#### BE D3 2023\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2023-baseball-d3/stats/general/all/batting ../csv/stats_batting_raw_d3_2023.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2023-baseball-d3/stats/general/all/pitching ../csv/stats_pitching_raw_d3_2023.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2023-baseball-d3/stats/general/all/fielding ../csv/stats_fielding_raw_d3_2023.csv
+
+
+printf "\n"
+printf "########\n"
+printf "## D4 ##\n"
+printf "########\n"
+
+printf "\n"
+printf "#### BE D4 2024\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d4/stats/general/all/batting ../csv/stats_batting_raw_d4_2024.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d4/stats/general/all/pitching ../csv/stats_pitching_raw_d4_2024.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2024-baseball-d4/stats/general/all/fielding ../csv/stats_fielding_raw_d4_2024.csv
+
+printf "\n"
+printf "#### BE D4 2023\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2023-baseball-d4/stats/general/all/batting ../csv/stats_batting_raw_d4_2023.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2023-baseball-d4/stats/general/all/pitching ../csv/stats_pitching_raw_d4_2023.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2023-baseball-d4/stats/general/all/fielding ../csv/stats_fielding_raw_d4_2023.csv
+
+printf "\n\n"
+printf "###########################\n"
+printf "# Belgium Softball Ladies #\n"
+printf "###########################\n"
+
+printf "\n"
+printf "##########\n"
+printf "## SDD1 ##\n"
+printf "##########\n"
+
+printf "\n"
+printf "#### BE SDD1 2024\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2024-softball-ladies-d1/stats/general/all/batting ../csv/stats_batting_raw_sdd1_2024.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2024-softball-ladies-d1/stats/general/all/pitching ../csv/stats_pitching_raw_sdd1_2024.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2024-softball-ladies-d1/stats/general/all/fielding ../csv/stats_fielding_raw_sdd1_2024.csv
+
+printf "\n"
+printf "#### BE SDD1 2023\n"
+#npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2023-softball-ladies-d1/stats/general/all/batting ../csv/stats_batting_raw_sdd1_2023.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2023-softball-ladies-d1/stats/general/all/pitching ../csv/stats_pitching_raw_sdd1_2023.csv
+#npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2023-softball-ladies-d1/stats/general/all/fielding ../csv/stats_fielding_raw_sdd1_2023.csv
+
+printf "\n"
+printf "#### BE SDD1 2022\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2022-softball-ladies-d1/stats/general/all/batting ../csv/stats_batting_raw_sdd1_2022.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2022-softball-ladies-d1/stats/general/all/pitching ../csv/stats_pitching_raw_sdd1_2022.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2022-softball-ladies-d1/stats/general/all/fielding ../csv/stats_fielding_raw_sdd1_2022.csv
+
+printf "\n"
+printf "##########\n"
+printf "## SDD2 ##\n"
+printf "##########\n"
+
+printf "\n"
+printf "#### BE SDD2 2024\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2024-softball-ladies-d2/stats/general/all/batting ../csv/stats_batting_raw_sdd2_2024.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2024-softball-ladies-d2/stats/general/all/pitching ../csv/stats_pitching_raw_sdd2_2024.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2024-softball-ladies-d2/stats/general/all/fielding ../csv/stats_fielding_raw_sdd2_2024.csv
+
+printf "\n"
+printf "#### BE SDD2 2023\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2023-softball-ladies-d2/stats/general/all/batting ../csv/stats_batting_raw_sdd2_2023.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2023-softball-ladies-d2/stats/general/all/pitching ../csv/stats_pitching_raw_sdd2_2023.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2023-softball-ladies-d2/stats/general/all/fielding ../csv/stats_fielding_raw_sdd2_2023.csv
+
+printf "\n"
+printf "##########\n"
+printf "## SDD3 ##\n"
+printf "##########\n"
+
+printf "\n"
+printf "#### BE SDD3 2024\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2024-softball-ladies-d3/stats/general/all/batting ../csv/stats_batting_raw_sdd3_2024.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2024-softball-ladies-d3/stats/general/all/pitching ../csv/stats_pitching_raw_sdd3_2024.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2024-softball-ladies-d3/stats/general/all/fielding ../csv/stats_fielding_raw_sdd3_2024.csv
+
+printf "\n"
+printf "#### BE SDD3 2023\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2023-softball-ladies-d3/stats/general/all/batting ../csv/stats_batting_raw_sdd3_2023.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2023-softball-ladies-d3/stats/general/all/pitching ../csv/stats_pitching_raw_sdd3_2023.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2023-softball-ladies-d3/stats/general/all/fielding ../csv/stats_fielding_raw_sdd3_2023.csv
+
+printf "\n\n"
+printf "###########################\n"
+printf "# Belgium Softball Men    #\n"
+printf "###########################\n"
+
+printf "\n"
+printf "##########\n"
+printf "## SMD1 ##\n"
+printf "##########\n"
+
+printf "\n"
+printf "#### BE SMD1 2024\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2024-softball-men-d1/stats/general/all/batting ../csv/stats_batting_raw_smd1_2024.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2024-softball-men-d1/stats/general/all/pitching ../csv/stats_pitching_raw_smd1_2024.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2024-softball-men-d1/stats/general/all/fielding ../csv/stats_fielding_raw_smd1_2024.csv
+
+printf "\n"
+printf "#### BE SMD1 2023\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2023-softball-men-d1/stats/general/all/batting ../csv/stats_batting_raw_smd1_2023.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2023-softball-men-d1/stats/general/all/pitching ../csv/stats_pitching_raw_smd1_2023.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2023-softball-men-d1/stats/general/all/fielding ../csv/stats_fielding_raw_smd1_2023.csv
+
+printf "\n"
+printf "#### BE SMD1 2022\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2022-softball-men-d1/stats/general/all/batting ../csv/stats_batting_raw_smd1_2022.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2022-softball-men-d1/stats/general/all/pitching ../csv/stats_pitching_raw_smd1_2022.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2022-softball-men-d1/stats/general/all/fielding ../csv/stats_fielding_raw_smd1_2022.csv
+
+printf "\n\n"
+printf "#############################\n"
+printf "# Belgium Softball Reserves #\n"
+printf "#############################\n"
+
+printf "\n"
+printf "##########\n"
+printf "## SRES ##\n"
+printf "##########\n"
+
+printf "\n"
+printf "#### BE SRES 2024\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2024-softball-men-reserves/stats/general/all/batting ../csv/stats_batting_raw_sres_2024.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2024-softball-men-reserves/stats/general/all/pitching ../csv/stats_pitching_raw_sres_2024.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2024-softball-men-reserves/stats/general/all/fielding ../csv/stats_fielding_raw_sres_2024.csv
+
+printf "\n"
+printf "#### BE SRES 2023\n"
+npm run webBattingScrapper https://www.baseballsoftball.be/en/events/2023-softball-men-reserves/stats/general/all/batting ../csv/stats_batting_raw_sres_2023.csv
+npm run webPitchingScrapper https://www.baseballsoftball.be/en/events/2023-softball-men-reserves/stats/general/all/pitching ../csv/stats_pitching_raw_sres_2023.csv
+npm run webFieldingScrapper https://www.baseballsoftball.be/en/events/2023-softball-men-reserves/stats/general/all/fielding ../csv/stats_fielding_raw_sres_2023.csv
+
+
+
+printf "\n\n"
+printf "\n\n"
 printf "#########################\n"
 printf "# FINISH                #\n"
 printf "#########################\n"
