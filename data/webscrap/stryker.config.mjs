6238dd89266ad45c9d3f610721acfed35e8772d3
Ernesto Navarro <ernesto.navarro@gmail.com>
2024-05-02 21:27:07 +0200

    Configure stryker

--- data/webscrap/stryker.config.mjs
+++ data/webscrap/stryker.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 /** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
 const config = {
-  _comment:
-    "This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.",
-  packageManager: "pnpm",
-  reporters: ["html", "clear-text", "progress"],
-  testRunner: "vitest",
-  testRunner_comment:
-    "Take a look at https://stryker-mutator.io/docs/stryker-js/vitest-runner for information about the vitest plugin.",
-  coverageAnalysis: "perTest",
-  plugins: ["@stryker-mutator/vitest-runner"],
+	_comment:
+			'This config was generated using \'stryker init\'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.',
+	packageManager: 'pnpm',
+	reporters: ['html', 'clear-text', 'progress'],
+	testRunner: 'vitest',
+	testRunner_comment:
+			'Take a look at https://stryker-mutator.io/docs/stryker-js/vitest-runner for information about the vitest plugin.',
+	coverageAnalysis: 'perTest',
+	plugins: ['@stryker-mutator/vitest-runner'],
+	mutate: ['src/interpreter/PlayerInterpreter.js'],
+	disableTypeChecks: '!examples/**'	
 };
 export default config;
