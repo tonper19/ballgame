96968330e6302eb6a4eaad9d251a6a7687d9781d
Tony Perez <tonper19@gmail.com>
2024-05-26 09:06:24 +0200

    Team standing dev

---
+++ data/webscrap/src/interpreter/TeamInterpreter.js
@@ -0,0 +1,36 @@
+export class TeamInterpreter {
+
+	_surNameMatcher = /<strong>(.*)<\/strong><br>\s(.*)/
+
+	constructor(container) {
+		this.container = container;
+	}
+
+	_getProperty(prop) {
+		return this.container.querySelector(prop)?.textContent;
+	}
+
+	_getPropertyAsHTML(prop) {
+		return this.container.querySelector(prop)?.innerHTML;
+	}
+
+	_playerNameCleanup(playerName){
+		return playerName?.replaceAll(',', ' ').replaceAll('  ',' ');
+	}
+
+	getPlayerSurName() {
+		const playerNameContainer = this._getPropertyAsHTML('.player');
+		const playerName = playerNameContainer?.match(this._surNameMatcher)?.[1];
+		return this._playerNameCleanup(playerName) || '-';
+	}
+
+	getPlayerFirstName() {
+		let playerNameContainer = this._getPropertyAsHTML('.player');
+		const playerName = playerNameContainer?.match(this._surNameMatcher)?.[2];
+		return this._playerNameCleanup(playerName) || '-';
+	}
+
+	getTeam() {
+		return this._getProperty('.team');
+	}
+}
